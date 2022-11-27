4dec8afa4e86a703ff784ebb5c3d2697a1ded008
Mariya Chernuhina <mariya.chernuhina@mail.ru>
2022-11-27 17:20:17 +0300

    Added UITabBarController, fnc and snc UINavigationControllers, FeedViewController, ProfileViewController

---
+++ Navigation/FeedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Мария on 25.11.2022.
+//
+
+import UIKit
+
+
+class FeedViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidload 1")
+        self.view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+
+
+        // self.createSecondNavigationController?.pushViewController(PostViewController, animated: true)
+       // PostViewController.titlePost = post.title
+    }
+
+
+
+
+}
+
---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  Navigation
+//
+//  Created by Мария on 25.11.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidload 2")
+        self.view.backgroundColor = .gray
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
